cffd2df3a134eb4fdb9a14c81986372cfa319df0
TheSoundOfWA <121793003+TheSoundOfWA@users.noreply.github.com>
2024-01-06 17:11:03 +0800

    Update the-best-time-to-buy-and-sell-stocks.java

--- book/top-interview-questions-easy/dp/the-best-time-to-buy-and-sell-stocks.java
+++ book/top-interview-questions-easy/dp/the-best-time-to-buy-and-sell-stocks.java
@@ -1,5 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int len = prices.length;
 
+        int havent = 0;
+        int have = -prices[0];
+        for (int i = 1; i < len; i++) {
+            havent = Math.max(havent, have + prices[i]);
+            have = Math.max(have, -prices[i]);
+        }
+        return havent;
     }
 }
